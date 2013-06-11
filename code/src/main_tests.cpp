339c13110ef374b92442fff51bc0c9870654df61
Lukas Murmann <lukas.murmann@gmail.com>
2013-06-11 11:28:44 +0100

    Implemented readSVG. Still some linker issues.

--- code/src/main_tests.cpp
+++ code/src/main_tests.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include "utils.h"
+#include "drawing.h"
+
 int
 main (int argc,
-      char **argv)
-{
-  return (0);
+      char **argv) {
+
+    if (argc != 2) {
+		fprintf(stderr, "Usage: %s FILE\n", argv[0]);
+		return 1;
+	}
+    std::string svg_file = argv[1];
+    veiv::Drawing drawing;
+    bool success = veiv::readSVG(svg_file, drawing);
+
+    return (!success);
 }
